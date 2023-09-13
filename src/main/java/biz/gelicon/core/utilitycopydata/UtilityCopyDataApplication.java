704c696db15e4874d924be52e1ce7b9e4daed558
MOBIUSinka <113015948+MOBIUSinka@users.noreply.github.com>
2023-09-13 11:32:14 +0500

    Add files via upload

---
+++ src/main/java/biz/gelicon/core/utilitycopydata/UtilityCopyDataApplication.java
@@ -0,0 +1,13 @@
+package biz.gelicon.core.utilitycopydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityCopyDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityCopyDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/gelicon/core/utilitycopydata/UtilityCopyDataApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.gelicon.core.utilitycopydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityCopyDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

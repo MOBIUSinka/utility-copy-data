4bfc821f3c1eff2cd3e2697265f2464f27b38164
Юшков Даниил <udv2@gelicon.biz>
2023-09-14 14:29:44 +0500

    add files

--- src/main/java/biz/gelicon/core/utilitycopydata/repository/DepartmentRepository.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/repository/DepartmentRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    @Override
+    boolean existsById(Long id);
 }

85d5b4bc32c7c19504b16cf5def3eea19d292d1f
Юшков Даниил <udv2@gelicon.biz>
2023-09-14 16:39:05 +0500

    add files

--- src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
@@ -1,14 +1,15 @@
 package biz.gelicon.core.utilitycopydata.controller;
 
-import biz.gelicon.core.utilitycopydata.mainrepository.DepartmentRepository;
+import biz.gelicon.core.utilitycopydata.mainmodel.MainDepartment;
+import biz.gelicon.core.utilitycopydata.mainrepository.MainDepartmentRepository;
 import biz.gelicon.core.utilitycopydata.mainrepository.ProjectRepository;
 import biz.gelicon.core.utilitycopydata.mainrepository.WorkGroupRepository;
 import biz.gelicon.core.utilitycopydata.mainrepository.WorkerRepository;
+import biz.gelicon.core.utilitycopydata.model.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 
-import javax.xml.crypto.Data;
 import java.util.List;
 
 @Controller
@@ -18,7 +19,7 @@ public class MainController {
     // - main db repositories
     //
     @Autowired
-    DepartmentRepository mainDepartmentRepository;
+    MainDepartmentRepository mainDepartmentRepository;
 
     @Autowired
     ProjectRepository mainProjectRepository;
@@ -51,10 +52,17 @@ public class MainController {
     // *
 
 
-    public void existenceCheckData(
-            JpaRepository<?, ?> mainRepo,
-            JpaRepository<?, ?> testRepo
-    ) {
-
+    public void copyData() {
+        List<Department> departmentList = departmentRepository.findAll();
+        for(Department department : departmentList) {
+            Integer departmentId = department.getDepartmentId();
+            if (!mainDepartmentRepository.existsById(departmentId)) {
+                MainDepartment mainDepartment = new MainDepartment();
+                mainDepartment.setDepartmentId(department.getDepartmentId());
+                mainDepartment.setDepartmentCode(department.getDepartmentCode());
+                mainDepartment.setDepartmentName(department.getDepartmentName());
+                mainDepartment.setDepartmentReportId(department.getDepartmentReportId());
+            }
+        }
     }
 }

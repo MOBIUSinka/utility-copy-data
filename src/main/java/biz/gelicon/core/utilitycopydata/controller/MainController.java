80a98e9108a0a141bb41550ade9f67418e40f8eb
Юшков Даниил <udv2@gelicon.biz>
2023-09-14 14:15:22 +0500

    add files

--- src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
@@ -1,9 +1,40 @@
 package biz.gelicon.core.utilitycopydata.controller;
 
+import biz.gelicon.core.utilitycopydata.mainrepository.DepartmentRepository;
+import biz.gelicon.core.utilitycopydata.mainrepository.ProjectRepository;
+import biz.gelicon.core.utilitycopydata.mainrepository.WorkGroupRepository;
+import biz.gelicon.core.utilitycopydata.mainrepository.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
 
+    // - main db repositories
+    @Autowired
+    DepartmentRepository mainDepartmentRepository;
 
+    @Autowired
+    ProjectRepository mainProjectRepository;
+
+    @Autowired
+    WorkerRepository mainWorkerRepository;
+
+    @Autowired
+    WorkGroupRepository mainWorkGroupRepository;
+
+
+
+    // - test db repositories
+    @Autowired
+    biz.gelicon.core.utilitycopydata.repository.DepartmentRepository departmentRepository;
+
+    @Autowired
+    biz.gelicon.core.utilitycopydata.repository.ProjectRepository projectRepository;
+
+    @Autowired
+    biz.gelicon.core.utilitycopydata.repository.WorkerRepository workerRepository;
+
+    @Autowired
+    biz.gelicon.core.utilitycopydata.repository.WorkGroupRepository workGroupRepository;
 }

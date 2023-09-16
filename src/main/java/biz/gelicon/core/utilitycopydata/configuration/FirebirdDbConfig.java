3c5fe405c0d50af274dacbabbd6859dc4154ec0b
Просвирнин Егор <pes@gelicon.biz>
2023-09-16 13:35:02 +0500

    Модель

--- src/main/java/biz/gelicon/core/utilitycopydata/configuration/FirebirdDbConfig.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/configuration/FirebirdDbConfig.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
 import javax.sql.DataSource;
 
 @Configuration

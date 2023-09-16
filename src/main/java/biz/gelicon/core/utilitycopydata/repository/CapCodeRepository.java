1b6504a6ebe96944dbf07e0e9916a7df7b1ca264
Просвирнин Егор <pes@gelicon.biz>
2023-09-16 15:09:11 +0500

    Модель

--- src/main/java/biz/gelicon/core/utilitycopydata/repository/CapCodeRepository.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/repository/CapCodeRepository.java
@@ -1,11 +1,11 @@
-package biz.gelicon.core.utilitycopydata.repository;
+    package biz.gelicon.core.utilitycopydata.repository;
 
 
-import biz.gelicon.core.utilitycopydata.model.CapCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+    import biz.gelicon.core.utilitycopydata.model.CapCode;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CapCodeRepository extends JpaRepository<CapCode, Long> {
-    boolean existsByCapCodeId(Integer CapcodeId);
-}
+    @Repository
+    public interface CapCodeRepository extends JpaRepository<CapCode, Long> {
+        boolean existsByCapCodeId(Integer CapCodeId);
+    }

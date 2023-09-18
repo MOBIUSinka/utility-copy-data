e31cdd029caeb0ff79c51a0857d86d79dbb525f1
Юшков Даниил <udv2@gelicon.biz>
2023-09-18 20:22:02 +0500

    add files

--- src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
+++ src/main/java/biz/gelicon/core/utilitycopydata/controller/MainController.java
@@ -163,59 +163,6 @@ public class MainController {
         }
     }
 
-//    @PostMapping ("/start-process-egor")
-//    public ResponseEntity<String> ErrorFinder() {
-//        List<String> failedOperations = new ArrayList<>(); // список возможных ошибок
-//
-//        CompletableFuture<Void> copyCapCode = CompletableFuture.runAsync(() -> {
-//            try {
-//                copyCapCodeData();
-//            } catch (Exception e) {
-//                failedOperations.add("Перенос данных CapCode: " + e.getMessage()); // если ошибка, то заносится в список
-//            }
-//        });
-//
-//        CompletableFuture<Void> copyCapCodeType = CompletableFuture.runAsync(() -> {
-//            try {
-//                copyCapCodeTypeData();
-//            } catch (Exception e) {
-//                failedOperations.add("Перенос данных CapCodeType: " + e.getMessage());
-//            }
-//        });
-//
-//        CompletableFuture<Void> copyProguser = CompletableFuture.runAsync(() -> {
-//            try {
-//                copyProguserData();
-//            } catch (Exception e) {
-//                failedOperations.add("Перенос данных Proguser: " + e.getMessage());
-//            }
-//        });
-//
-//        CompletableFuture<Void> copyProguserGroup = CompletableFuture.runAsync(() -> {
-//            try {
-//                copyProguserGroupData();
-//            } catch (Exception e) {
-//                failedOperations.add("Перенос данных ProguserGroup: " + e.getMessage());
-//            }
-//        });
-//
-//        CompletableFuture<Void> allOf2 =
-//                CompletableFuture.allOf(copyCapCode, copyCapCodeType, copyProguser, copyProguserGroup); // всевозможные
-//
-//        try {
-//            allOf2.get();
-//            if (!failedOperations.isEmpty()) {
-//                return ResponseEntity.badRequest()
-//                        .body("Не удалось выполнить: \n" + String.join("\n", failedOperations));
-//            } else {
-//                return ResponseEntity.ok("Все данные были успешно перенесены. ");
-//            }
-//        } catch (InterruptedException | ExecutionException e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body("Произошла ошибка при выполнении операций: " + e.getMessage());
-//        }
-//    }
-
     // Юшков
     public void copyDepartmentData() {
         List<Department> departmentList = departmentRepository.findAll();
@@ -280,16 +227,6 @@ public class MainController {
         }
         return fioBuilder.toString();
     }
-
-//    public String searchWorkerTabNumber(
-//            String family,
-//            String firstname,
-//            String surname,
-//            Integer reportId
-//    ) {
-//            String fio = (family.charAt(0) + firstname.charAt(0) + surname.charAt(0) + String.valueOf(reportId));
-//            return fio;
-//    }
 
     // узнать пол по отчеству
     public static Integer getGenderByPatronymic(String patronymic) {
